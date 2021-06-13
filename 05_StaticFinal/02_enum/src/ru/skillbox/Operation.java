bbc209645f724753aaafbc74ad0299d60d2a3331
Вадим <elrodan92@gmail.com>
2021-06-13 16:00:02 +0300

    Домашнее задание 5.2 правки

--- 05_StaticFinal/02_enum/src/ru/skillbox/Operation.java
+++ 05_StaticFinal/02_enum/src/ru/skillbox/Operation.java
@@ -1,19 +1,5 @@
 package ru.skillbox;
 
 public enum Operation {
-    ADD {
-        public int add(int a, int b) {
-            return a + b;
-        }
-    },
-    SUBTRACT {
-        public int subtract(int a, int b) {
-            return a - b;
-        }
-    },
-    MULTIPLY {
-        public int multiply(int a, int b) {
-            return a * b;
-        }
-    }
+    ADD, SUBTRACT, MULTIPLY
 }
