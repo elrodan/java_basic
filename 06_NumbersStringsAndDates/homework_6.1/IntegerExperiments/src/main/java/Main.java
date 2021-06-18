797b84bbcf48c2d48a0a21cda7edcf27e7f0f89b
Вадим <elrodan92@gmail.com>
2021-06-18 11:14:19 +0300

    Домашнее задание 6.1 с поправкой

--- 06_NumbersStringsAndDates/homework_6.1/IntegerExperiments/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.1/IntegerExperiments/src/main/java/Main.java
@@ -2,14 +2,14 @@ package main.java;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Container container = new Container();
-    container.count += 7843;
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.count += 7843;
 
-    int sum = sumDigits(7843);
+        int sum = sumDigits(7843);
 
-    System.out.println(sum);
-  }
+        System.out.println(sum);
+    }
 
   /* Реализуйте метод sumDigits который возвращает сумму цифр числа, пример:
   передано 12345, метод должен вернуть 15
@@ -21,15 +21,15 @@ public class Main {
   В противном случае тестовый метод не сможет проверить ваш код
    */
 
-  public static int sumDigits(Integer number) {
-    int sum = 0;
-    if (number == null) {
-      return -1;
-    } else {
-      for (int i = 0; i < number.toString().length(); i++) {
-        sum += Integer.parseInt(String.valueOf(number.toString().charAt(i)));
-      }
-      return sum;
+    public static int sumDigits(Integer number) {
+        int sum = 0;
+        if (number == null) {
+            return -1;
+        } else {
+            for (int i = 0; i < number.toString().length(); i++) {
+                sum += Integer.parseInt(String.valueOf(number.toString().charAt(i)));
+            }
+            return sum;
+        }
     }
-  }
 }
