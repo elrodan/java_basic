1b059b17189cf445e8d55189205b1f5c19dc13c2
Вадим <elrodan92@gmail.com>
2021-06-28 21:29:10 +0300

    Домашняя работа 6.5 задание 1

--- 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,21 @@
+package main.java;
+
+import java.util.regex.*;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    System.out.println(calculateSalarySum("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей"));
   }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+    Pattern pattern = Pattern.compile("(\\d+)");
+    Matcher matcher = pattern.matcher(text);
+    int totalSum = 0;
+    while (matcher.find()) {
+      totalSum += Integer.parseInt(matcher.group(1));
+    }
+    return totalSum;
   }
 
 }
--- 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/test/java/TestEarningsCalculator.java
+++ 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/test/java/TestEarningsCalculator.java
@@ -1,3 +1,5 @@
+package test.java;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -8,6 +10,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import main.java.Main;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
