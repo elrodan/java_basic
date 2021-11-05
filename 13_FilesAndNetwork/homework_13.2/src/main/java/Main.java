a82458094f497ca723f42b31f5adedb14e320e26
Вадим <elrodan92@gmail.com>
2021-11-05 07:29:26 +0300

    Домашняя работа 13.2

--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,20 @@
+package main.java;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь до папки, которую хотите скопировать:");
+        String srcPath = scanner.nextLine();
+        System.out.println("Введите путь до папки, в которую хотите скопировать:");
+        String destPath = scanner.nextLine();
+        try {
+            FileUtils.copyFolder(srcPath, destPath);
+            System.out.println("Копирование завершено!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
