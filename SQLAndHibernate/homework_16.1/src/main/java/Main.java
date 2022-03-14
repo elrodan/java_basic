8d0b184ebf7677743c4c046b0170883a6f2e5a2c
Вадим <elrodan92@gmail.com>
2022-03-14 20:19:24 +0300

    16.6 Практическая работа №2

--- SQLAndHibernate/homework_16.1/src/main/java/Main.java
+++ SQLAndHibernate/homework_16.1/src/main/java/Main.java
@@ -1,32 +1,21 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/skillbox";
-        String user = "root";
-        String pass = "qwerty123";
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, pass);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select course_name, " +
-                    "count(course_name)/(max(month(subscription_date)) - min(month(subscription_date)) + 1) as average_count_sale " +
-                    "from purchaselist where year(subscription_date) = 2018 " +
-                    "group by course_name order by course_name");
-            while (resultSet.next()) {
-                System.out.format("%-34s\t%8s\n",
-                        resultSet.getString("course_name"),
-                        resultSet.getString("average_count_sale"));
-            }
-            resultSet.close();
-            statement.close();
-            connection.close();
+        Session session = sessionFactory.openSession();
+        Course course = session.get(Course.class, 3);
+        System.out.println("Название курса: " + course.getName() +
+                "\nКоличество студентов: " + course.getStudentsCount());
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        sessionFactory.close();
     }
 }
